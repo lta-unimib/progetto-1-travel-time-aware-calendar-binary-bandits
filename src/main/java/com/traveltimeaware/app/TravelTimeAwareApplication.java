a756c16209a3367f757fded90a9c7c332ef97cfe
Davide Creati <dadicreati@gmail.com>
2023-02-07 16:06:41 +0100

    Create project with Spring Boot and Gradle

---
+++ src/main/java/com/traveltimeaware/app/TravelTimeAwareApplication.java
@@ -0,0 +1,13 @@
+package com.traveltimeaware.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelTimeAwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelTimeAwareApplication.class, args);
+	}
+
+}
