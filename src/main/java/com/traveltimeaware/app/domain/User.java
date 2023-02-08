4128776f91d7f030e69ab4f861d92133ddf07a26
DESKTOP-I8S17BQ\dadic <dadicreati@gmail.com>
2023-02-08 11:56:01 +0100

    Add Object method

--- src/main/java/com/traveltimeaware/app/domain/User.java
+++ src/main/java/com/traveltimeaware/app/domain/User.java
@@ -1,12 +1,14 @@
 package com.traveltimeaware.app.domain;
 
+import java.util.Objects;
+
 public class User {
     private final String name;
     private final String surname;
     private String email;
     private String password;
     
-    public User (String name, String surname, String email, String password) {
+    public User(String name, String surname, String email, String password) {
     	this.name = name;
     	this.surname = surname;
     	this.email = email;
@@ -28,4 +30,28 @@ public class User {
     public void updateEmail(String email) {
     	this.email = email;
     }
+
+	@Override
+	public String toString() {
+		return "User [name=" + name + ", surname=" + surname + ", email=" + email + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(email);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(email, other.email);
+	}
+    
+    
 }
