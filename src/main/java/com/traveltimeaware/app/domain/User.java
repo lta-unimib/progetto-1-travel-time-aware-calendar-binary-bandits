612cbd21b0d7d4e97c1e4bd1a89224e67794cf6f
DESKTOP-I8S17BQ\dadic <dadicreati@gmail.com>
2023-02-08 11:54:31 +0100

    Update User class. Add constructor, make it semi-mutable and ovrerride Object method

--- src/main/java/com/traveltimeaware/app/domain/User.java
+++ src/main/java/com/traveltimeaware/app/domain/User.java
@@ -1,29 +1,31 @@
 package com.traveltimeaware.app.domain;
 
 public class User {
-    private String name;
-    private String surname;
+    private final String name;
+    private final String surname;
     private String email;
     private String password;
+    
+    public User (String name, String surname, String email, String password) {
+    	this.name = name;
+    	this.surname = surname;
+    	this.email = email;
+    	this.password = password;
+    }
 
     public String getName() {
         return name;
     }
-    public void setName(String name) {
-        this.name = name;
-    }
+
     public String getSurname() {
         return surname;
     }
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
+    
     public String getEmail() {
         return email;
     }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     
+    public void updateEmail(String email) {
+    	this.email = email;
+    }
 }
