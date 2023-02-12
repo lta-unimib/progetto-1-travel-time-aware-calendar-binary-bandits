a816f5c1ca66e243e8552d54d3253fef3efed8dd
DESKTOP-I8S17BQ\dadic <dadicreati@gmail.com>
2023-02-12 15:39:19 +0100

    Add Bing Maps key

--- src/main/java/com/traveltimeaware/app/TravelTimeAwareApplication.java
+++ src/main/java/com/traveltimeaware/app/TravelTimeAwareApplication.java
@@ -2,10 +2,11 @@ package com.traveltimeaware.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class TravelTimeAwareApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TravelTimeAwareApplication.class, args);
 	}
