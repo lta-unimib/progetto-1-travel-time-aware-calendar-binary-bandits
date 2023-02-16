fc5fa326d956e9dcc96a723db4863d0cb22b2f66
DESKTOP-I8S17BQ\dadic <dadicreati@gmail.com>
2023-02-16 19:04:42 +0100

    remove dedug section

--- src/main/java/com/traveltimeaware/app/TravelTimeAwareApplication.java
+++ src/main/java/com/traveltimeaware/app/TravelTimeAwareApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-import com.traveltimeaware.app.util.BingMapsRequest;
 import com.traveltimeaware.app.util.CalendarInitializer;
 
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
@@ -16,24 +15,11 @@ public class TravelTimeAwareApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TravelTimeAwareApplication.class, args);
 		
-		/*try {
+		try {
 			CalendarInitializer.start();
 		} catch (IOException | GeneralSecurityException e) {
 			e.printStackTrace();
-		} */
-		
-		BingMapsRequest r = null;
-		try {
-			r = new BingMapsRequest.Builder("/Routes/Driving")
-					.param("wp.0", "redmond,wa")
-					.param("wp.1", "Issaquah,wa")
-					.param("avoid", "minimizeTolls").build();
-			
-			System.out.println(r.getResponse());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
+		} 
 	}
 
 }
