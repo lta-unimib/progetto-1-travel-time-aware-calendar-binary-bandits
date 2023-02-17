79c5f14c30f824665168ccf2e2b925c2a57d1708
dadec <dadec@DESKTOP-GOLUP9K.fritz.box>
2023-02-17 11:43:00 +0100

    update conflit

--- src/main/java/com/traveltimeaware/app/TravelTimeAwareApplication.java
+++ src/main/java/com/traveltimeaware/app/TravelTimeAwareApplication.java
@@ -9,17 +9,32 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 import com.traveltimeaware.app.util.CalendarInitializer;
 
+import java.sql.*;
+
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class TravelTimeAwareApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TravelTimeAwareApplication.class, args);
+
+		//connessione al database
+		/*String urlDatabase = "url del database";
+		String usernameDatabase = "username";
+		String passwordDatabase = "password";
+		
+		try {
+			Connection connectionDatabase = DriverManager.getConnection(urlDatabase, usernameDatabase, passwordDatabase);
+			System.out.println("Connessione riuscita");
+			
+		} catch (SQLException e) {
+			System.out.println("Connessione non riuscita");
+		}*/
 		
 		try {
 			CalendarInitializer.start();
 		} catch (IOException | GeneralSecurityException e) {
 			e.printStackTrace();
-		}
+		} 
 	}
 
 }
