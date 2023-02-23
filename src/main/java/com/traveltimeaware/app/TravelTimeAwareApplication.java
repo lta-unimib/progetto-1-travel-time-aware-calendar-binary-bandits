8f0d3fb99295fbfd199d9e961a7673da34a1fd81
DESKTOP-GOLUP9K\dadec <dadicreati@gmail.com>
2023-02-23 15:38:13 +0100

    remove unused variables

--- src/main/java/com/traveltimeaware/app/TravelTimeAwareApplication.java
+++ src/main/java/com/traveltimeaware/app/TravelTimeAwareApplication.java
@@ -1,26 +1,13 @@
 package com.traveltimeaware.app;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 
 
 // user@email.com/pass
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
-@Configuration
 public class TravelTimeAwareApplication {
-	
-	private String urlDatabase;
-
-	private String usernameDatabase;
-
-	private String passwordDatabase;
-
-	private String portDatabase;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TravelTimeAwareApplication.class, args);
