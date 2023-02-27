a044312331416c80cce45c9000307bd6d327ee3e
DESKTOP-GOLUP9K\dadec <dadicreati@gmail.com>
2023-02-27 18:30:03 +0100

    add data to response

--- src/main/java/com/traveltimeaware/app/controller/CalendarController.java
+++ src/main/java/com/traveltimeaware/app/controller/CalendarController.java
@@ -1,13 +1,14 @@
 package com.traveltimeaware.app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.traveltimeaware.app.domain.Calendar;
 import com.traveltimeaware.app.domain.repo.CalendarRepository;
-import com.traveltimeaware.app.security.domain.User;
+import com.traveltimeaware.app.security.CustomUserDetails;
 
 @Controller
 public class CalendarController {
@@ -17,23 +18,21 @@ public class CalendarController {
 	
 	@GetMapping("/")
 	public ModelAndView getHomepage() {
+		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+		String email = "";
+		if (principal instanceof CustomUserDetails) {
+			email = ((CustomUserDetails) principal).getUsername();
+		} else {
+			email = principal.toString();
+		}
+		
+		Calendar c = calendarRepo.findByEmail(email);
+		
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("calendar");
-		// mv.getModel().put("data", "data");
+		mv.getModel().put("days", c.getDays());
 		
-		return mv;
-	}
-	
-	@GetMapping("/provac")
-	public ModelAndView tryed() {
-		Calendar c = calendarRepo.findByEmail("user@email.com");
-		
-		System.out.print(c.getDays());
-		
-		ModelAndView mv = new ModelAndView();
-		mv.setViewName("signup");
-		mv.getModel().put("user", new User());
-	
 		return mv;
 	}
 }
