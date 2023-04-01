e61c398af858a1d4fc1552ffa05790a84c4a311d
DESKTOP-GOLUP9K\dadec <dadicreati@gmail.com>
2023-04-01 12:48:42 +0200

    Add SQLite configuration

---
+++ src/main/java/com/traveltimeaware/app/config/SQLiteConfig.java
@@ -0,0 +1,32 @@
+package com.traveltimeaware.app.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+@Profile("sqlite")
+@EnableJpaRepositories(basePackages = "com.traveltimeaware.app.repository")
+@PropertySource("classpath:persistence.properties")
+public class SQLiteConfig {
+	
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public DataSource dataSource() {
+        final DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getProperty("driverClassName"));
+        dataSource.setUrl(env.getProperty("url"));
+        dataSource.setUsername(env.getProperty("user"));
+        dataSource.setPassword(env.getProperty("password"));
+        return dataSource;
+    }
+}
